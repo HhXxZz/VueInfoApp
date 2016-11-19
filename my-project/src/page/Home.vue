6e245ea63d58bb758f538b6d5b096b3a9a63ea89
dxl <775046211@qq.com>
2016-11-19 16:59:04 +0800

    init

---
+++ my-project/src/page/Home.vue
@@ -0,0 +1,43 @@
+<style>
+
+</style>
+<template>
+<div>
+<!-- Drawer 抽屉 -->
+<div>
+<mu-drawer :open="open" :docked="docked" @close="toggle()">
+      <mu-list @itemClick="docked ? '' : toggle()">
+        <mu-list-item title="Menu Item 1"/>
+        <mu-list-item title="Menu Item 2"/>
+        <mu-list-item title="Menu Item 3"/>
+        <mu-list-item v-if="docked" @click.native="open = false" title="Close"/>
+      </mu-list>
+</mu-drawer>
+
+<div>
+  <home-index></home-index>
+</div>
+
+</div>
+</template>
+<script>
+ import homeIndex from './HomeIndex'
+export default {
+  name: 'home',
+  data () {
+    return {
+      open: false,
+      docked: true
+    }
+  },
+  components:{
+      homeIndex
+  },
+  methods:{
+    toggle (flag) {
+      this.open = !this.open
+      this.docked = !flag
+    }
+  }
+}
+</script>
---
+++ my-project/src/page/HomeIndex.vue
@@ -0,0 +1,33 @@
+<style>
+  .mu-menu-item{
+    width: 120px;
+  }
+</style>
+
+<template>
+  <div>
+  <mu-appbar title="Title">
+    <mu-icon-button icon='menu' slot="left" @click="openDraw"/>
+    <mu-icon-menu icon="more_vert" slot="right">
+      <mu-menu-item class="mu-menu-item" title="夜间模式"/>
+      <mu-menu-item class="mu-menu-item" title="设置选项"/>
+    </mu-icon-menu>
+  </mu-appbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+    openDraw(){
+      this.$parent.toggle(true)
+    }
+  }
+}
+</script>
